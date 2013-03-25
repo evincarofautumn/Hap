5891ad6bef26217e6183eb4009c497460e5ea18b
Jon Purdy <evincarofautumn@gmail.com>
2013-03-24 18:17:58 -0700

    Start testing parser.

--- unit/main.cpp
+++ unit/main.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 void suite_tokenize();
+void suite_parse();
 
 int main(int argc, char** argv) try {
   suite_tokenize();
+  suite_parse();
 } catch (const exception& error) {
   cerr << error.what() << '\n';
   return 1;
---
+++ unit/parse.cpp
@@ -0,0 +1,61 @@
+#include "BlockStatement.h"
+#include "Parser.h"
+#include "tokenize.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+using namespace hap;
+using namespace std;
+
+#define TEST_PARSE(NAME, INPUT, EXPECTED) \
+  test_parse(__FILE__, __LINE__, NAME, INPUT, EXPECTED)
+
+void test_parse
+  (const string& file,
+   const int line,
+   const string& name,
+   const string& input,
+   const shared_ptr<Statement> expected) {
+  shared_ptr<Statement> actual;
+  {
+    const shared_ptr<Environment> environment(new Environment());
+    istringstream stream(input);
+    const auto tokens(tokenize(stream));
+    Parser parser(tokens, environment);
+    actual = parser.accept_program();
+  }
+  if (*actual == *expected) {
+    cout << "Unit test '" << name << "' passed.\n";
+    return;
+  }
+  ostringstream message;
+  message
+    << file << ":" << line
+    << ": Unit test '" << name << "' failed:\n"
+    << "Expected:\n" << *expected
+    << "Actual:\n" << *actual
+    << '\n';
+  throw runtime_error(message.str());
+}
+
+void suite_parse() {
+  {
+    shared_ptr<Statement> expected(new BlockStatement());
+    TEST_PARSE
+      ("empty program",
+       "",
+       expected);
+  }
+  {
+    shared_ptr<BlockStatement> block(new BlockStatement());
+    block->push(shared_ptr<Statement>(new BlockStatement()));
+    shared_ptr<Statement> expected
+      (static_pointer_cast<Statement>(block));
+    TEST_PARSE
+      ("empty statement",
+       ";",
+       expected);
+  }
+}
